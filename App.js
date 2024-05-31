a998b6330f46159108d8e2a7ee9e2747d4289cb7
sachinKumar8050 <sachinpal8050101@gmail.com>
2024-05-31 14:30:50 +0530

    update

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welText}>Welcome</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'gray',
+  },
+  welText: {
+    color: 'red',
+    fontWeight: '600',
+    fontSize: 20,
+  },
+});
